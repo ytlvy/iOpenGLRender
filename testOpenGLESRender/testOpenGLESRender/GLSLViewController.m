f700f7038a14ae4ce6fdd7cb770daba5547802a8
gyj <yanjie.guo@kuwo.cn>
2023-09-11 22:37:35 +0800

    init

---
+++ testOpenGLESRender/testOpenGLESRender/GLSLViewController.m
@@ -0,0 +1,39 @@
+//
+//  GLSLViewController.m
+//  testOpenGLESRender
+//
+//  Created by Lyman on 2019/2/21.
+//  Copyright © 2019 Lyman Li. All rights reserved.
+//
+
+
+
+#import "GLSLViewController.h"
+#import "GLSLBox.h"
+
+
+@interface GLSLViewController ()
+
+@property(nonatomic, strong) GLSLBox *box;
+
+@end
+
+@implementation GLSLViewController
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.box];
+}
+
+- (GLSLBox *) box {
+    if(!_box) {
+        _box = [[GLSLBox alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    }
+    return _box;
+}
+
+@end
